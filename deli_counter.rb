62e7c54f4d0be2ac326fde1fcbdb480fa0ea9d85
John Guest <jcguest90@gmail.com>
2019-08-19 16:26:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,8 @@ def line(array)
   else 
      puts "The line is currently "
     array.each_with_index do |name, num|
-      new_arry << "#{num + 1}. #{name}"
+    new_arry << "#{num + 1}. #{name}"
+    
      
       
     end

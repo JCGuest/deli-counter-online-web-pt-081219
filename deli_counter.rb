3a83972acbed229c98e578fd757b7fc1a7fca7d0
John Guest <jcguest90@gmail.com>
2019-08-19 16:07:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def line(array)
+  new_arry = []
+  if array.length == 0 
+  puts "The line is currently empty."
+  else 
+    array.each_with_index do |name, num|
+      new_arry << "#{num + 1}. #{name}"
+    end
+  end
+end
